af626cc10d04236299989e4264434830dda0d5d3
cad-masters <aaron@cadmasters.com>
2020-05-11 14:14:11 -0700

    5-11-hardcoded-vals

---
+++ MobilityMatters/MobilityMatters.Web/Modules/Northwind/Order/OrderShipCity.cs
@@ -0,0 +1,33 @@
+﻿
+namespace MobilityMatters.Northwind.Lookups
+{
+    using Entities;
+    using Serenity.ComponentModel;
+    using Serenity.Data;
+    using Serenity.Web;
+
+    [LookupScript]
+    public class OrderShipCityLookup : RowLookupScript<Entities.OrderRow>
+    {
+        public OrderShipCityLookup()
+        {
+            IdField = TextField = OrderRow.Fields.ShipCity.PropertyName;
+        }
+
+        protected override void PrepareQuery(SqlQuery query)
+        {
+            var fld = Entities.OrderRow.Fields;
+            query.Distinct(true)
+                
+                .Select(fld.ShipCity)
+                .Where(
+                    
+                    new Criteria(fld.ShipCity) != "" &
+                    new Criteria(fld.ShipCity).IsNotNull());
+        }
+
+        protected override void ApplyOrder(SqlQuery query)
+        {
+        }
+    }
+}
